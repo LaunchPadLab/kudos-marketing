a365a72caea90e9bcdd7b92d9cd50951f2afdf5e
Monique Marchwiany <monique@launchpadlab.com>
2018-04-26 17:21:07 -0500

    updates to marketing site

--- _site/javascript/main.js
+++ _site/javascript/main.js
@@ -1,1 +1,3 @@
 //= require delay-animation
+//= require slider
+//= require scroll
--- javascript/slider.js
+++ javascript/slider.js
@@ -12,3 +12,21 @@
       }
     });
   });
+
+  $(document).ready(function() {
+  $('.article-owl-carousel').owlCarousel({
+    center:true,
+    autoplay: true,
+    loop: true,
+    margin:10,
+    nav: true,
+    responsive:{
+      0:{
+          items:1
+      },
+      700:{
+        items:3
+      }
+    }
+  });
+});
